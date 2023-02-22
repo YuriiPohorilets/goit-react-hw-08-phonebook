d1b707433809b236fb71a6e57023a65c86931945
YuriiPohorilets <pohorilets.y@gmail.com>
2023-02-22 20:02:02 +0200

    added title home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,8 @@ export const HomePage = () => {
       <Title>Your phonebook application</Title>
       <Paragraph>
         What was the name of that person? A unified phone book with integrated keyword search is
-        extremely helpful when this question comes up.
+        extremely helpful when this question comes up. Wherever, whenever. Your phone book is always
+        at hand.
       </Paragraph>
       {!isLoggedIn && (
         <Paragraph>
